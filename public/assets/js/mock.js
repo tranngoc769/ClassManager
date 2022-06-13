5cbd406162a1dcf72b7365f2eb17d753eeed9aff
tnquang769 <tnquang769@gmail.com>
2022-06-14 00:21:05 +0700

    mock test

---
+++ public/assets/js/mock.js
@@ -0,0 +1,9 @@
+function fill_add_user() {
+    $("#is_lead").val("1");
+    $("#tele_id").val("1234");
+    $("#tele_user").val("qua");
+    $("#address").val("123 TC");
+    $("#social").val("fb.com");
+    $("#phone").val("034888");
+    $("#full_name").val("Nguyen Van C");
+}
